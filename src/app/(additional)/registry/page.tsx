72c08f7661025b13b6c9f4ad49fc8ed25e8360f0
Andrew Hunn <ahunn59@gmail.com>
2025-09-16 12:22:44 -0400

    registry

--- src/app/(additional)/registry/page.tsx
+++ src/app/(additional)/registry/page.tsx
@@ -11,71 +11,52 @@ export default async function RegistryPage() {
     <>
       <Card className="mb-8 border-2 border-[#8b1a1a]/20 bg-white/85">
         <CardHeader>
-          <CardTitle className="font-madi text-center text-5xl text-[#8b1a1a]">
+          <CardTitle className="text-center font-madi text-5xl text-[#8b1a1a]">
             Wedding Registry
           </CardTitle>
         </CardHeader>
         <CardContent className="text-center text-gray-600">
           <p className="leading-relaxed">
             Your presence at our wedding is the greatest gift we could ask for.
-            However, if you wish to honor us with a gift, we have created
-            registries at the following locations:
+            However, if you wish to honor us with a gift, we have created a
+            registry below.
           </p>
         </CardContent>
       </Card>
 
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {/* Amazon Registry */}
-        <Card className="border-2 border-[#8b1a1a]/20 bg-white/85">
+        {/* The Knot Registry */}
+        <Card className="border-2 border-[#8b1a1a]/20 bg-white/85 md:col-span-full">
           <CardHeader>
-            <CardTitle className="font-madi text-center text-4xl text-[#8b1a1a]">
-              Amazon
+            <CardTitle className="text-center font-madi text-4xl text-[#8b1a1a]">
+              The Knot Registry
             </CardTitle>
           </CardHeader>
           <CardContent className="text-center text-gray-600">
             <p className="mb-6">
-              Find a variety of items from our wishlist, including kitchen
-              essentials, home decor, and everyday items.
+              We&apos;ve created a registry on The Knot. If you would like to
+              contribute, please click the button below. Thanks so much :D{" "}
+            </p>
+            <p>
+              We are truly grateful for your love, support, and generosity. Your
+              presence at our wedding and your thoughtful gifts mean the world
+              to us as we begin this new chapter in our lives.
             </p>
             <a
-              href="#"
+              href="https://registry.theknot.com/jade-mckinney-andrew-hunn-december-2025-mi/70747725"
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-block rounded-full bg-[#8b1a1a]/10 px-6 py-2 text-[#8b1a1a] transition-all hover:bg-[#8b1a1a]/20"
+              className="mt-5 inline-block rounded-full bg-[#8b1a1a] px-8 py-3 text-lg font-bold text-white shadow-lg ring-2 ring-[#8b1a1a]/30 transition-all duration-200 hover:scale-105 hover:bg-[#a32d2d] hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-[#8b1a1a]/50"
             >
-              View Amazon Registry
-            </a>
-          </CardContent>
-        </Card>
-        {/* Zola Registry */}
-        <Card className="border-2 border-[#8b1a1a]/20 bg-white/85 md:col-span-2">
-          <CardHeader>
-            <CardTitle className="font-madi text-center text-4xl text-[#8b1a1a]">
-              Zola Registry
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="text-center text-gray-600">
-            <p className="mb-6">
-              We&apos;ve created a registry on Zola that includes both home
-              essentials and a honeymoon fund. Your contribution to either will
-              Your contribution to either will help us build our home together
-              or create lasting memories during our honeymoon adventures.
-            </p>
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block rounded-full bg-[#8b1a1a]/10 px-6 py-2 text-[#8b1a1a] transition-all hover:bg-[#8b1a1a]/20"
-            >
-              View Zola Registry
+              View The Knot Registry
             </a>
           </CardContent>
         </Card>
 
         {/* Thank You Note - Span full width */}
-        <Card className="border-2 border-[#8b1a1a]/20 bg-white/85 md:col-span-2 lg:col-span-3">
+        {/* <Card className="border-2 border-[#8b1a1a]/20 bg-white/85 md:col-span-2 lg:col-span-3">
           <CardHeader>
-            <CardTitle className="font-madi text-center text-4xl text-[#8b1a1a]">
+            <CardTitle className="text-center font-madi text-4xl text-[#8b1a1a]">
               Thank You
             </CardTitle>
           </CardHeader>
@@ -86,7 +67,7 @@ export default async function RegistryPage() {
               to us as we begin this new chapter in our lives.
             </p>
           </CardContent>
-        </Card>
+        </Card> */}
       </div>
     </>
   );
